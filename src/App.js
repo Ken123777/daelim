b5b495094e896fa8521ad091e714f6744e09a82a
Ken Huh <ken@vanillacoding.co>
2020-07-14 16:00:10 +0900

    1st session

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // const result = useState(0);
+  // const count = result[0];
+  // const setCount = result[1];
+
+  const [ count, setCount ] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        현재 카운트는 {count}입니다.
+      </div>
+      <button onClick={() => setCount(count + 1)}>클릭하시면 카운트가 증가해요!</button>
+    </>
   );
 }
 
