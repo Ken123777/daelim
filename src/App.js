fa4c422910a0d579347771ab9cbbc47f07eb6bd6
Ken Huh <ken@vanillacoding.co>
2020-07-20 15:50:10 +0900

    Day 2 Add problem detail page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { getProblemList } from './api/problems';
+import CodeMirror from 'react-codemirror';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript';
+
+// prop
+function ProblemDetail({ problem }) {
+  return (
+    <div className="problem">
+      <section className="description">
+        <h3>{problem.title}</h3>
+        <p>{problem.description}</p>
+      </section>
+      <section className="code-editor">
+        <CodeMirror
+          value={'function solution () {}'}
+          options={{
+            mode: 'javascript'
+          }}
+        />
+        <button>제출</button>
+      </section>
+    </div>
+  );
+}
 
 function App() {
-  // const result = useState(0);
-  // const count = result[0];
-  // const setCount = result[1];
+  const [ problems, setProblems ] = useState([]);
+  const [ selectedProblemId, setSelectedProblemId ] = useState(null);
 
-  const [ count, setCount ] = useState(0);
+  const [ selectedProblem ] = problems.filter(data => data.id === selectedProblemId);
+
+  useEffect(function () {
+    async function getProblems () {
+      const data = await getProblemList();
+      setProblems(data);
+    }
+
+    getProblems();
+  }); // dependency...
 
   return (
-    <>
-      <div className="App">
-        현재 카운트는 {count}입니다.
-      </div>
-      <button onClick={() => setCount(count + 1)}>클릭하시면 카운트가 증가해요!</button>
-    </>
+    <div className="App">
+      {
+        selectedProblemId === null &&
+        <>
+          <nav>
+            Codewars
+          </nav>
+          <ul>
+            {
+              problems.map(function (problem) {
+                return (
+                  <li key={problem.id}>
+                    <h3>{problem.title}</h3>
+                    <button onClick={() => setSelectedProblemId(problem.id)}>문제풀기</button>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </>
+      }
+      {
+        selectedProblemId !== null &&
+        <ProblemDetail problem={selectedProblem} />
+      }
+    </div>
   );
 }
 
