89b0c9354b56431611b624bfcd2bab2e200a8a0e
Ken Huh <ken@vanillacoding.co>
2021-12-13 09:39:50 +0000

    Update hashTable.js

--- src/hashTable.js
+++ src/hashTable.js
@@ -1,11 +1,11 @@
 /*
  *
  * 본인이 구현하는 메소드의 시간 복잡도에 대해서 생각해보세요.
- * 새로운 메소드나 속성은 추가해야 하는 행위는 신중히 결정하세요.
+ * 새로운 메소드나 속성을 추가하는 행위는 신중히 결정하세요.
  *
  */
 
-function HashTable() {
+function HashTable () {
   this._limit = 8;
   this._storage = createLimitedArray(this._limit);
 }
